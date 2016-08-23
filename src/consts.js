5a0c5e67baa2685c042d4b233fb732e62de9663f
Patrick Golden <ptgolden0@gmail.com>
2016-08-23 12:47:45 -0400

    Add "view" parameter to open refine settings

--- src/consts.js
+++ src/consts.js
@@ -7,7 +7,10 @@ const PERIOD_TYPE = {
 
 const METADATA = {
   name: 'Periodo reconciliation service',
-  defaultTypes: { period: PERIOD_TYPE }
+  defaultTypes: { period: PERIOD_TYPE },
+  view: {
+    url: "http://n2t.net/ark:/99152/{{id}}"
+  }
 }
 
 module.exports = { PERIOD_TYPE, METADATA }
